4598d91ac2c30940788549248d7780caca9e01dd
enne <enne.august@gmail.com>
2022-12-01 09:18:46 +0800

    A

---
+++ js/basic/basic.js
@@ -0,0 +1,12 @@
+// 
+
+var year, yearDulguun, yearTuul;
+
+let Year = 2022;
+let ageDulguun = 5;
+let YearDulguun = Year - ageDulguun;
+
+console.log('Dulguunii torson jil bol ' + YearDulguun);
+
+console.log(32 ** 5 );
+var johnOlder = ageDulguun
