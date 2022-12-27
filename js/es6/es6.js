f3cb89ecf2230564de972875a79bdee66aca9158
enktl822 <morning@admins-iMac.local>
2022-12-27 12:45:28 +0800

    push

---
+++ js/es6/es6.js
@@ -0,0 +1,42 @@
+// function greet() {
+//   console.log("Hello there!");
+// }
+// greet();
+
+//  -----------------------------------------------------
+
+// function greet(name) {
+//   console.log("Hello " + name + " :)");
+// }
+
+// let input = prompt("Write your name : ");
+
+// greet(input);
+
+//  -----------------------------------------------------
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// add(3, 4);
+// add(5, 8);
+
+//  ------------------------------------------------------
+
+// let x = function (num) {
+//   return num * num;
+// };
+
+// console.log(x(4));
+
+//  -------------------------------------------------------
+
+// let y = x(3);
+// console.log(y);
+
+//  -------------------------------------------------------
+
+// let b = (num) => {
+//   return num * num;
+// };
